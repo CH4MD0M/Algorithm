4f1c8b4b39f8f0a99c6d3aa5e6389263d45b0559
ch4md0m <started677@gmail.com>
2022-03-25 14:44:29 +0900

    정렬 연습문제

---
+++ lecture/Sort/6-10.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+arr = sorted(arr, reverse=True)
+
+print(*arr)
---
+++ lecture/Sort/6-11.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = []
+
+for _ in range(n):
+    data = input().split()
+    arr.append((data[0], data[1]))
+
+arr = sorted(arr, key=lambda x: x[1])
+
+for i in arr:
+    print(i[0], end=' ')

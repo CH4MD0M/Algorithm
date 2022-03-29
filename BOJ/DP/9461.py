01b78b186fb60cf74f1b5e3292be356012a34420
ch4md0m <started677@gmail.com>
2022-03-29 17:46:19 +0900

    코드 수정

--- BOJ/DP/9461.py
+++ BOJ/DP/9461.py
@@ -1,13 +1,13 @@
-T=int(input())
-dp=[0]*101
-dp[1]=1
-dp[2]=1
-dp[3]=1
+t = int(input())
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
 
-for i in range(0,98):
-    dp[i+3]=dp[i]+dp[i+1]
+for i in range(0, 98):
+    dp[i + 3] = dp[i] + dp[i + 1]
 
-for _ in range(T):
-    N=int(input())
-    print(dp[N])
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
 

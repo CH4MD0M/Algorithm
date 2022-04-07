a92dd058434c66c9024584bd231188ced6d5b7bd
KiHoon Roh <started677@gmail.com>
2022-04-07 20:57:43 +0900

    체육복 (Python, JS)

---
+++ Programmers/Level1/체육복/체육복.js
@@ -0,0 +1,16 @@
+function solution(n, lost, reserve) {
+    let _lost = lost.filter((v) => !reserve.includes(v));
+    let _reserve = reserve.filter((v) => !lost.includes(v));
+
+    for (let i = 0; i < _reserve.length; i++) {
+        if (_lost.includes(_reserve[i] - 1)) {
+            _lost = _lost.filter((v) => v !== _reserve[i] - 1);
+        } else if (reserve.includes(lost[i] + 1)) {
+            _lost = _lost.filter((v) => v !== _reserve[i] + 1);
+        }
+    }
+    return n - _lost.length;
+}
+
+// console.log(n - _lost.length);
+solution(5, [2, 4], [3]);

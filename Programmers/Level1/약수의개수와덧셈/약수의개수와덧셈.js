af4b3ad69c18d253fb6c435faa8f225bd9e28f03
KiHoon Roh <started677@gmail.com>
2022-04-13 23:00:33 +0900

    약수의 개수와 덧셈 (Python, JS)

---
+++ Programmers/Level1/약수의개수와덧셈/약수의개수와덧셈.js
@@ -0,0 +1,11 @@
+function solution(left, right) {
+    let answer = 0;
+    for (let i = left; i < right + 1; i++) {
+        let count = 0;
+        for (let j = 1; j < i + 1; j++) {
+            if (i % j === 0) count += 1;
+        }
+        answer = count % 2 === 0 ? answer + i : answer - i;
+    }
+    return answer;
+}

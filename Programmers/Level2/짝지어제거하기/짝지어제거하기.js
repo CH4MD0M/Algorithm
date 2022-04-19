e1a00161116cb59ea5010230dda44fc7b6d798a0
KiHoon Roh <started677@gmail.com>
2022-04-19 20:33:45 +0900

    짝지어 제거하기 (Python, JS)

---
+++ Programmers/Level2/짝지어제거하기/짝지어제거하기.js
@@ -0,0 +1,23 @@
+function solution(s) {
+    let stack = [];
+
+    for (let i of s) {
+        if (stack.length == 0) stack.push(i);
+        else if (stack[stack.length - 1] == i) stack.pop();
+        else stack.push(i);
+    }
+
+    return stack.length ? 0 : 1;
+}
+
+// short 풀이
+function solution(s) {
+    let stack = [];
+
+    for (let i of s) {
+        stack[stack.length - 1] == i ? stack.pop() : stack.push(i);
+    }
+
+    return stack.length ? 0 : 1;
+}
+solution("cdcd");

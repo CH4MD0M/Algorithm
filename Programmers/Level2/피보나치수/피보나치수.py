37b2601f40b13ad6ee52d01a41b6595a91d27cfb
KiHoon Roh <started677@gmail.com>
2022-05-23 16:56:06 +0900

    피보나치 수 (Python)

---
+++ Programmers/Level2/피보나치수/피보나치수.py
@@ -0,0 +1,9 @@
+def solution(n):
+    d = [0 for _ in range(n+1)]
+    d[1] = 1
+    d[2] = 1
+    
+    for i in range(3, n + 1):
+        d[i] = (d[i - 1] + d[i - 2]) % 1234567
+        
+    return d[n] 

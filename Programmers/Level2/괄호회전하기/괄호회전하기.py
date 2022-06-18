793d34d8f709609e2c53fb22cdc63de8d69e5876
KiHoon Roh <started677@gmail.com>
2022-06-18 22:10:39 +0900

    ⚙️ 프로그래머스 Level2 다시 풀기 (3일차)

--- Programmers/Level2/괄호회전하기/괄호회전하기.py
+++ Programmers/Level2/괄호회전하기/괄호회전하기.py
@@ -6,48 +6,45 @@ def solution(s):
 
     for _ in range(len(s)):
         stack = []
-        for i in range(len(queue)):
+        for i in queue:
             if not stack:
-                stack.append(queue[i])
-                continue     
-            if stack[-1] == '[' and queue[i] == ']':
+                stack.append(i)
+            elif stack[-1] == '[' and i == ']':
                 stack.pop()
-            elif stack[-1] == '(' and queue[i] == ')':
+            elif stack[-1] == '(' and i == ')':
                 stack.pop()
-            elif stack[-1] == '{' and queue[i] == '}':
+            elif stack[-1] == '{' and i == '}':
                 stack.pop()
             else:
-                stack.append(queue[i])
+                stack.append(i)
 
         queue.rotate(-1)    
         
-        if not stack:
-            answer += 1
+        if not stack: answer += 1
             
     return answer
 
 # deque 라이브러리 미사용
 def solution(s):
     answer = 0
-    lst = list(s)
-
+    s = list(s)
+    
     for _ in range(len(s)):
         stack = []
-        for i in range(len(lst)):
+        for i in s:
             if not stack:
-                stack.append(queue[i])
-                continue         
-            if stack[-1] == '[' and lst[i] == ']':
+                stack.append(i)
+            elif stack[-1] == '[' and i == ']':
                 stack.pop()
-            elif stack[-1] == '(' and lst[i] == ')':
+            elif stack[-1] == '(' and i == ')':
                 stack.pop()
-            elif stack[-1] == '{' and lst[i] == '}':
+            elif stack[-1] == '{' and i == '}':
                 stack.pop()
             else:
-                stack.append(lst[i])
+                stack.append(i)
 
-        lst.append(lst.pop(0))
-        if not stack:
-            answer += 1
-            
+        s.append(s.pop(0))
+        
+        if not stack: answer += 1
+        
     return answer
--- Programmers/Level2/위장/위장.py
+++ Programmers/Level2/위장/위장.py
@@ -1,13 +1,26 @@
 def solution(clothes):
-    _dic = {}
+    clothes_dic = {}
     answer = 1
     for c in clothes:
-        if c[1] in _dic.keys():
-            _dic[c[1]].append(c[0])
+        if c[1] in clothes_dic.keys():
+            clothes_dic[c[1]].append(c[0])
         else:
-            _dic[c[1]] = [c[0]]
+            clothes_dic[c[1]] = [c[0]]
             
-    for v in _dic.values():
+    for v in clothes_dic.values():
         answer *= len(v) + 1
 
     return answer - 1    
+
+# 다른 풀이
+def solution(clothes):
+    answer = 1
+    clothe_dict = {}
+    
+    for clothe, kind in clothes:
+        clothe_dict[kind] = clothe_dict.get(kind, 0) + 1
+    
+    for kind in clothe_dict:
+        answer *= (clothe_dict[kind] + 1)
+    
+    return answer - 1

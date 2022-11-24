4eca54718d56994beba4925c6974d7a2e8741ef9
KiHoon Roh <started677@gmail.com>
2022-11-24 23:04:04 +0900

    [다시풀기] [PG]문자열압축 (JS)

--- Programmers/Level2/문자열압축/문자열압축.js
+++ Programmers/Level2/문자열압축/문자열압축.js
@@ -1,21 +1,22 @@
 function solution(s) {
-    let answer = s.length;
-    for (let i = 1; i < s.length / 2 + 1; i++) {
-        let count = 1;
-        let tmp = s.slice(0, i);
-        let check = "";
+  let answer = s.length;
 
-        for (let j = i; j < s.length; j += i) {
-            if (tmp === s.slice(j, j + i)) {
-                count += 1;
-            } else {
-                check += count != 1 ? String(count) + tmp : tmp;
-                tmp = s.slice(j, j + i);
-                count = 1;
-            }
-        }
-        check += count != 1 ? String(count) + tmp : tmp;
-        answer = Math.min(answer, check.length);
+  for (let step = 1; step <= s.length / 2; step++) {
+    let count = 1;
+    let prev = s.slice(0, step);
+    let compressed = '';
+
+    for (let i = step; i < s.length; i += step) {
+      if (prev === s.slice(i, i + step)) {
+        count++;
+      } else {
+        compressed += count >= 2 ? String(count) + prev : prev;
+        count = 1;
+        prev = s.slice(i, i + step);
+      }
     }
-    return answer;
+    compressed += count >= 2 ? String(count) + prev : prev;
+    answer = Math.min(answer, compressed.length);
+  }
+  return answer;
 }
